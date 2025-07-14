c88a0ae13f25932c00de1a36aafdb27a7fc6bc81
Yuktha mukhi <164024317+yuktham357@users.noreply.github.com>
2025-07-14 16:24:23 +0530

    Add files via upload

---
+++ ArrayIntialization.java
@@ -0,0 +1,17 @@
+--------------1D Array Initialization-------------------
+class Main {
+    public static void main(String[] args) {
+        int[] x= new int[5][3];
+        x[0]=1;
+        x[1]=2;
+        x[2]=3;
+        x[3]=4;
+        x[4]=5;
+        for(int i=0;i<5;i++){
+            System.out.println("Element at index " +  x[i]);
+        }
+        
+    }
+}
+
+---------------2D Array Initialization-------------------
